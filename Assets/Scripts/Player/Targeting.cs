998eb16c5a8893a0f67f033c815fbcc0431bd1ab
Felix Waiblinger <diebreze@gmail.com>
2023-06-20 00:02:35 +0200

    added smoothing to targeting

--- Assets/Scripts/Player/Targeting.cs
+++ Assets/Scripts/Player/Targeting.cs
@@ -2,9 +2,20 @@ using UnityEngine;
 
 public class Targeting : MonoBehaviour
 {
+    private Vector3 _targetPosition = Vector3.forward * 5;
+    private Quaternion _targetRotation = Quaternion.identity;
+    private Vector3 _start;
+
+    void Update()
+    {
+        var player = transform.parent.position + Vector3.up;
+        transform.position = Vector3.MoveTowards(transform.position, player + _targetPosition, 1);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, _targetRotation, 1);
+    }
+
     public void SetTargetDirection(Vector3 direction)
     {
-        transform.position = transform.parent.position + Vector3.up + direction.normalized * 5;
-        transform.rotation = Quaternion.LookRotation(direction);
+        _targetPosition = direction.normalized * 5;
+        _targetRotation = Quaternion.LookRotation(direction);
     }
 }
